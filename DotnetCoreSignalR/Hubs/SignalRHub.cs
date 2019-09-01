ea7eb62200de8283685dd4ea283c317f1389dc55
Ersin SECER <ersinsecer@hotmail.com.tr>
2019-09-01 16:12:08 +0300

    Proje dosyası ekle.

---
+++ DotnetCoreSignalR/Hubs/SignalRHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreSignalR.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task SendMessageToUser(string connectionId, string message)
+        {
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(ex);
+        }
+    }
+}
---
+++ DotnetCoreSignalR/wwwroot/js/SignalR.js
@@ -0,0 +1,54 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/signalrhub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var div = document.createElement("div");
+    div.innerHTML = msg + "<hr/>";
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.on("UserConnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    var option = document.createElement("option");
+    option.text = connectionId;
+    option.value = connectionId;
+    groupElement.add(option);
+});
+
+connection.on("UserDisconnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    for (var i = 0; i < groupElement.length; i++) {
+        if (groupElement.options[i].value === connectionId) {
+            groupElement.remove(i);
+        }
+    }
+});
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    debugger;
+    var message = document.getElementById("messageInput").value;
+    var groupElement = document.getElementById("group");
+    var groupValue = groupElement.options[groupElement.selectedIndex].value;
+
+    if (groupValue === "All" || groupValue === "Myself") {
+        var method = groupValue === "All" ? "SendMessage" : "SendMessageToCaller";
+        connection.invoke(method, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    } else {
+        connection.invoke("SendMessageToUser", groupValue, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } 
+});
